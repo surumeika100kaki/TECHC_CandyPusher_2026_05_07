c5ad98acf1dac12379b848101ae55e65f331e305
kantaro0914 <surumeika100kaki@gmail.com>
2026-05-14 18:13:58 +0900

    物理演算追加
    物理演算を追加し、pusherがcandyを押して動かせるようにしました。

--- Assets/Scripts/Move_Pusher.cs
+++ Assets/Scripts/Move_Pusher.cs
@@ -2,25 +2,12 @@ using UnityEngine;
 
 public class Move_Pusher : MonoBehaviour
 {
-    //int value;
-    //bool[] bools;
-    //int[] num;
     public float speed = 1f;
     public float pusherMoveRange = 5f;
     private Vector3 startPosition;
     // Start is called on1ce before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //num = new int[10];
-        //bools = new bool[10];
-        //Debug.Log("Hello World!");
-        //num[0] = 1;
-        //for (int i = 1; i < 10; i++)
-        //{
-        //    num[i] = num[i - 1] + num[i - 1];
-        //    Debug.Log(num[i]);
-        //}
-
         startPosition = this.transform.localPosition;
     }
 
@@ -28,7 +15,7 @@ public class Move_Pusher : MonoBehaviour
     void Update()
     {
         float z = Mathf.Sin(Time.time * speed) * pusherMoveRange;
-        //自身のローカル座標の位置を最初の位置情報にz(sin波の変動値)を科さんして返す
+        //自身のローカル座標の位置を最初の位置情報にz(sin波の変動値)を加算して返す
         this.transform.localPosition = startPosition + new Vector3(0, 0, z);
     }
 }
