af6735dda4ec005c690c244c861feb856c1683c7
kantaro0914 <surumeika100kaki@gmail.com>
2026-05-21 17:43:20 +0900

    [変更]プッシャーの移動システム変更
    プッシャーの移動システムをRigidBodyを使った、移動方式へ変更しました。

--- Assets/Scripts/Move_Pusher.cs
+++ Assets/Scripts/Move_Pusher.cs
@@ -4,23 +4,24 @@ public class Move_Pusher : MonoBehaviour
 {
     public float speed = 1f;
     public float pusherMoveRange = 5f;
-    public GameObject Pusher;
     private Vector3 startPosition;
-    public Vector3 endPosition;
+    private Rigidbody rb ;
+
     // Start is called on1ce before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Debug.Log("Hello World");
         startPosition = this.transform.localPosition;
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.angularVelocity = new Vector3(0,0,speed);
-
+        //自身についているRigidbodyコンポーメントを取得して、変数rbに入れる。
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //float z = Mathf.Sin(Time.time * speed) * pusherMoveRange;
+        float z = Mathf.Sin(Time.time * speed) * pusherMoveRange;
         //自身のローカル座標の位置を最初の位置情報にz(sin波の変動値)を加算して返す
         //this.transform.localPosition = startPosition + new Vector3(0, 0, z);
+        rb.linearVelocity = new Vector3(0,0,z);
     }
 }
