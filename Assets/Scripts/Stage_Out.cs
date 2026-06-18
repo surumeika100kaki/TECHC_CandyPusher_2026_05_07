efe161b87a41b6f28ac51ff881951dde61fbe92d
kantaro0914 <surumeika100kaki@gmail.com>
2026-06-18 18:37:24 +0900

    フォント整理とDropLogUIの追加
    フォントのファイルの整理をしました
    落ちたCandyの名前がログUIに流れるようにしました

--- Assets/Scripts/Stage_Out.cs
+++ Assets/Scripts/Stage_Out.cs
@@ -1,21 +1,37 @@
+using System.Collections.Generic;
+using System.Threading;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stage_Out : MonoBehaviour
 {
     //スコアの保存変数の作成
     private int Score = 0;
-    [SerializeField] TextMeshProUGUI Score_Text;
+
+    List<string> DropCandyList = new List<string>(){};
+
+    private int MaxLangDropCandyLog = 6;
+    [SerializeField] TextMeshProUGUI Score_TMP;
+    [SerializeField] Text Score_Txte_Leg;
+
+    [SerializeField] TextMeshProUGUI DropCandyLog;
+
     //このコードをアタッチしたオブジェクトに他のオブジェクトがすり抜けた時に呼ばれる。
     private void Start()
     {
-        Score_Text.text = $"スコア:{Score}";
+        Score_TMP.text = $"スコア:{Score}";
+
+        Score_Txte_Leg.text = $"スコア:{Score}";
+
+        DropCandyLog.text = "ログ:\n" + string.Join("\n",DropCandyList);
         //別解
         //Score_Text.text = "スコア：" + Score;
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{other.name}がすり抜けた");
+        //Debug.Log($"{other.name}がすり抜けた");
         //すり抜けたオブジェクトを破棄
         Destroy( other.gameObject );
         //スコアを+1追加
@@ -23,7 +39,19 @@ public class Stage_Out : MonoBehaviour
         //別解
         //Score += 1;
         //コンソールにスコアを表示
-        Debug.Log($"スコア：{Score}");
-        Score_Text.text = $"スコア:{Score}";
+        //Debug.Log($"スコア：{Score}");
+        Score_TMP.text = $"スコア:{Score}";
+        Score_Txte_Leg.text = $"スコア:{Score}";
+
+        if(DropCandyList.Count > MaxLangDropCandyLog)
+        {
+            DropCandyList.RemoveAt(0);
+            DropCandyList.Add(other.name);
+        }
+        else
+        {
+            DropCandyList.Add(other.name);
+        }
+        DropCandyLog.text = "ログ:\n" + string.Join("\n",DropCandyList);;
     }
 }
