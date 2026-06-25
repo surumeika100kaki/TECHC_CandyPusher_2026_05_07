72a13914a43ff046d7376186819c5d47408dbeb5
surumeika100kaki <surumeika100kaki@gmail.com>
2026-06-25 18:51:32 +0900

    [add]Candy生成器動き追加
    candyの生成器に動きを追加しました
    
    z移動は製作途中

--- Assets/Scripts/CreateCandy.cs
+++ Assets/Scripts/CreateCandy.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 
 public class CreateCandy : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 2*Mathf.PI;
     public float CreateCandy_MoveRange_x = 5f;
     public float CreateCandy_MoveRange_z = 5f;
     private Vector3 stastartPosition;
+
+    private int move_count = 0;
 
     private int CandyCount = 0;
 
@@ -20,12 +22,11 @@ public class CreateCandy : MonoBehaviour
 
         void Update()
     {
-        float z = Mathf.Sin(Time.time * speed) * CreateCandy_MoveRange_x;
-        float x = Mathf.Sin(Time.time * speed) * CreateCandy_MoveRange_z;
-        //this.transform.position = stastartPosition + new Vector3(x, 0, 0);
-        this.transform.position = stastartPosition + new Vector3(x, 0, z);
+        float z = stastartPosition.z;
+        float x = Mathf.Sin(Time.time * speed) * CreateCandy_MoveRange_x;
+        z = Mathf.Sin(Time.time * speed) * CreateCandy_MoveRange_z;
 
-
+        this.transform.position = stastartPosition+new Vector3(x,0,z);
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             AddCandy();
