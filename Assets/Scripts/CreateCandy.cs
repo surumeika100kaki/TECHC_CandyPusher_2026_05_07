9a08f8328cb1262bd45ced234c43d44469da4735
surumeika100kaki <138195971+surumeika100kaki@users.noreply.github.com>
2026-06-27 18:47:09 +0900

    CreateCabdyの動き完成
    CreateCandyを毎秒xを1往復。2~3の間にxとzを一往復するように変更しました。

--- Assets/Scripts/CreateCandy.cs
+++ Assets/Scripts/CreateCandy.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class CreateCandy : MonoBehaviour
 {
-    public float speed = 2*Mathf.PI;
-    public float CreateCandy_MoveRange_x = 5f;
-    public float CreateCandy_MoveRange_z = 5f;
-    private Vector3 stastartPosition;
-
-    private int move_count = 0;
+    //往復までの速度を一秒にする
+    private float speed = 2*Mathf.PI;
+    private float CreateCandy_MoveRange_x = 2.5f;
+    private float CreateCandy_MoveRange_z = 2.5f;
+    private Vector3 startPosition;
+    public TextMeshProUGUI Count_Timer;
 
     private int CandyCount = 0;
 
@@ -17,33 +18,56 @@ public class CreateCandy : MonoBehaviour
 
     void Start()
     {
-        stastartPosition = transform.position;
+        startPosition = transform.position;
     }
 
         void Update()
     {
-        float z = stastartPosition.z;
-        float x = Mathf.Sin(Time.time * speed) * CreateCandy_MoveRange_x;
-        z = Mathf.Sin(Time.time * speed) * CreateCandy_MoveRange_z;
-
-        this.transform.position = stastartPosition+new Vector3(x,0,z);
+        MoveCandyGenerate();
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             AddCandy();
         }
+        Count_Timer.text = (Time.time % 3).ToString();
         //if (Input.GetKeyDown(KeyCode.Space))
         //{
         //    AddCandy();
         //}
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0.2f;
+        }
+
+        // Oキーで通常速度に戻す (等倍速)
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Time.timeScale = 1.0f;
+        }
     }
 
     private void AddCandy()
     {
         CandyCount++;
         Debug.Log(CandyCount);
-        //�ʉ�
         //CandyCount = CandyCount + 1;
         GameObject createPrefab = Instantiate(Candy[Random.Range(0,Candy.Count)]);
         createPrefab.transform.position = this.transform.position;
+    }
+
+    private void MoveCandyGenerate()
+    {
+        float z = 0;
+        float x = Mathf.Sin(Time.time * speed)*CreateCandy_MoveRange_x;
+
+        if (Time.time % 3 > 2)
+        {
+            z = Mathf.Sin(Time.time * speed)*CreateCandy_MoveRange_z;
+            Debug.Log($"A _ time % 3 = {Time.time % 3} _ z = {z} _ x = {x}");
+        }else
+        {
+            Debug.Log($"B _ time % 3 = {Time.time % 3} _ z = {z} _ x = {x}");
+        }
+        this.transform.position = startPosition+new Vector3(x,0,z);    
     }
 }
